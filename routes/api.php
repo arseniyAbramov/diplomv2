646c1ccc3515efcad090887c32444b98269a3123
arseniyAbramov <arseniy2281337214@gmail.com>
2025-05-14 03:22:43 +0300

    feat(auth): реализован logout через Sanctum и защита маршрута /user

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -15,13 +16,20 @@ Route::post('/message', function (Request $request) {
     ]);
 });
 
-
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json([
         'status' => 'ok',
         'user' => $request->user(),
+    ]);
+});
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+
+    return response()->json([
+        'status' => 'logged_out'
     ]);
 });
